d17ed1a5f9058b1752cc1fc65d90c84d0a068cbb
Javier Melo <108475704+javiermelosofka@users.noreply.github.com>
2024-08-10 11:21:13 -0500

    Initial Commit

---
+++ PokemonApi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PokemonApi
+//
+//  Created by Javier Duvan Hospital Melo on 10/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PokemonApi/PokemonApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonApiApp.swift
+//  PokemonApi
+//
+//  Created by Javier Duvan Hospital Melo on 10/08/24.
+//
+
+import SwiftUI
+
+@main
+struct PokemonApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
